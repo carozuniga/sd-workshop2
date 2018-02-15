9854cb88413e28a6d2d54cf2b65f4411e939461e
Laura <lauraaragon1995@gmail.com>
2018-02-14 19:40:49 -0500

    added receive and send

---
+++ A00315292/send.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import pika
+credentials=pika.PlainCredentials('sensu','password')
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='192.168.131.102',virtual_host='/sensu',credentials=credentials))
+channel = connection.channel()
+
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Caro')
+print(" [x] Sent 'Hello World!'")
+connection.close()
+
